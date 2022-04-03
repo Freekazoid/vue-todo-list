d2725ef3d26e550f71b8966f5a523887c3bc3234
Freekazoid <web-26@ya.ru>
2022-04-03 17:19:29 +0300

    add description task

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <h2>Реализовать to-do лист.</h2>
+
+    <p>Требования:</p>
+    <ul>
+      <li>Использовать router-view</li>
+      <li>Использовать store, минимум логики в компонентах и view</li>
+      <li>Добавление новой задачи по нажатию кнопки Enter</li>
+      <li>При нажатии на задачу - делать её выполненной</li>
+      <li>Отображать счётчик задач</li>
+      <li>Кнопки фильтров: Все, выполненные, активные</li>
+      <li>Кнопка clear completed если хотя бы одна задача выполнена</li>
+      <li>При перезагрузке страницы задачи и их статусы не должны сбрасываться</li>
+    </ul>
   </div>
 </template>
 
@@ -11,8 +25,11 @@ export default {
 </script>
 <style scoped>
 .home{
+  widows: 100%;
+  height: 80vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 }
 </style>
